c743e1295f47fe4aa9e3dcb3cb6cceda6391d3af
liam-patel1 <liam.patel@city.ac.uk>
2024-11-19 10:59:24 +0000

    added hw2 and hw3

--- HW 5/task1.sql
+++ HW 5/task1.sql
@@ -7,7 +7,9 @@ WHERE cust_name = "Bob Crow";
 -- 2
 
 SELECT DISTINCT cust_name
-FROM restBill;
+FROM restBill
+WHERE cust_name LIKE "% Smith"
+ORDER BY cust_name DESC;
 
 -- 3
 
@@ -15,7 +17,24 @@ SELECT DISTINCT cust_name
 FROM restBill
 WHERE cust_name LIKE '% C%';
 
+
 -- 4
 
-SELECT head
+SELECT DISTINCT headwaiters.first_name, headwaiters.surname
+FROM restStaff
+INNER JOIN restStaff headwaiters
+ON headwaiters.staff_no = restStaff.headwaiter
+WHERE headwaiters.staff_no = restStaff.headwaiter;
 
+-- 5
+
+SELECT *
+FROM restBill
+WHERE bill_date LIKE "1602__";
+
+-- 6
+
+SELECT DISTINCT bill_date
+FROM restBill
+WHERE bill_date LIKE "15____"
+ORDER BY bill_date ASC;
---
+++ HW 5/task3.sql
@@ -0,0 +1,4 @@
+-- 1 
+
+SELECT first_name, surname
+FROM 
